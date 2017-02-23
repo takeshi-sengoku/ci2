e458d5dde8dce0ca20db768b1e3008f73d6a3964
takeshi sengoku <takeshi_sengoku@precs.co.jp>
2017-02-23 11:47:35 +0900

    1

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+class triple_a {
+	var single_a = 'a';
+
+	function double_a () {
+		return 'aa';
+	}
+}
+
+class triple_b {
+	var single_a = 'a';
+
+	function double_a () {
+		return 'aa';
+	}
+}
+
+triple_a::double_a();
+$b = new triple_b;
+$b->double_a();
